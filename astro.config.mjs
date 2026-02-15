8a3f7501a0565abe85533d52004471d72c8ecae5
Nalla <nani.rama1982@gmail.com>
2026-02-15 09:59:36 +0530

    Add wranger

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+   output: "static",
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://screwfast.uk",
   image: {
